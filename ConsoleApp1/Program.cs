a96cbf5d6425f092ccf176174698b1326687c0ae
Danylo Omelia <danylo_omelia@epam.com>
2021-10-13 13:27:39 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //TODO: todo
+        static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+
+    }
+}
